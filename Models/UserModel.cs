0a5b2bf3d3a8e7b376d4f1a491bc81b302952df1
agrapes22 <agrapes22@gmail.com>
2021-09-16 20:44:17 -0700

    Data Validation and Database
    Added database connection string and data validation to Index view for UserModel.

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -11,5 +11,36 @@ namespace Milestone.Models
      */
     public class UserModel
     {
+        //First Name, Last Name, Sex, Age, State, Email Address, Username, and Password
+
+        [Required]
+        [DisplayName("First Name:")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name:")]
+        public string LastName { get; set; }
+
+        [DisplayName("Sex:")]
+        public string Sex { get; set; }
+
+        [DisplayName("Age:")]
+        public string Age { get; set; }
+
+        [Required]
+        [DisplayName("State:")]
+        public string State { get; set; }
+
+        [Required]
+        [DisplayName("Email Address:")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DisplayName("Username:")]
+        public string Username { get; set; }
+
+        [Required]
+        [DisplayName("Password:")]
+        public string Password { get; set; }
     }
 }
---
+++ Service/SecurityDA.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Milestone.Service
+{
+    public class SecurityDA
+    {
+
+        string connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MilestoneDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public bool findUser(User user)
+        {
+            bool found = false;
+            string query = "Select * from [dbo].[Users] where Username = @username and Password = @password";
+
+            using (SqlConnection connect = new SqlConnection(connectString))
+            {
+                SqlCommand command = new SqlCommand(query, connect);
+
+                command.Parameters.Add("@USERNAME", System.Data.SqlDbType.Varchar, 50).Value = user.UserName;
+                command.Parameters.Add("@PASSWORD", System.Data.SqlDbType.Varchar, 50).Value = user.Password;
+
+                try
+                {
+                    connect.Open();
+                    SqlDataReader read = command.ExecuteReader();
+
+                    if(read.HasRows)
+                    {
+                        found = true;
+                    }
+                }
+                catch (Exception ex)
+
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return found;
+        }
+    }
+}
