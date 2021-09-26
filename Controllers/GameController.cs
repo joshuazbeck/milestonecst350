efe8dd47adae9dbd00eb2ed303d3192ac0473a47
tray15 <tray15@my.gcu.edu>
2021-09-26 11:14:14 -0700

    added game grid
    added the game grid
    pictures and further styling needed

---
+++ Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milestone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Milestone.Controllers
+{
+    public class GameController : Controller
+    {
+        static List<CellModel> cells = new List<CellModel>();
+        Random rand = new Random();
+        static int GRID_SIZE = 36;
+        public IActionResult Index()
+        {
+            cells = new List<CellModel>();
+
+            for (int i = 0; i < GRID_SIZE; i++)
+            {
+                cells.Add(new CellModel(i, rand.Next(2)));
+            }
+            return View("Index", cells);
+        }
+    }
+}
---
+++ Models/CellModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Milestone.Models
+{
+    public class CellModel
+    {
+        public int Id { get; set; }
+        public int State { get; set; }
+
+        public CellModel ()
+        {
+
+        }
+
+        public CellModel (int id, int state)
+        {
+            Id = id;
+            State = state;
+        }
+    }
+}
