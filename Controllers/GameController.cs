d0050b355c4b02d0926faf53e39fba5cc0a6f78f
OfWonder <47732866+OfWonder@users.noreply.github.com>
2021-10-03 09:44:22 -0700

    Working on the styling. Also fixed the game logic for flood fill and bomb count.

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -12,23 +12,41 @@ namespace Milestone.Controllers
         static List<CellModel> cells = new List<CellModel>();
 
         Random rand = new Random();
-        static int GRID_SIZE = 36;
+        static int GRID_SIZE = 6;
         static bool gameStarted = false;
         static bool gameOver = false;
         public IActionResult Index()
         {
             if (!gameStarted)
             {
-                int x = (int)Math.Sqrt(GRID_SIZE);
+                int x = GRID_SIZE;
                 int y = x;
                 int id = 0;
+                double difficulty = .1;
+                int bombCount = (int)((double)difficulty * (GRID_SIZE * GRID_SIZE));
+                int randRow, randCol;
+                CellModel currCell;
 
                 for (int i = 0; i < x; i++)
                 {
                     for (int j = 0; j < y; j++)
                     {
-                        cells.Add(new CellModel(id, rand.Next(2), i, j));
+                        cells.Add(new CellModel(id, 0, i, j));
                         id++;
+                    }
+                }
+
+                while (bombCount > 0)
+                {
+                    randRow = rand.Next(x + 1);
+                    randCol = rand.Next(y + 1);
+
+                    currCell = cells.Find(cell => cell.Row == randRow && cell.Col == randCol);
+
+                    if (currCell != null)
+                    {
+                        currCell.State = 1;
+                        bombCount--;
                     }
                 }
 
@@ -94,12 +112,15 @@ namespace Milestone.Controllers
                 currCell.Visited = true;
             }
 
-            int[] xmove = { -1, -1, -1, 0, 0, 1, 1, 1 };
-            int[] ymove = { -1, -0, 1, -1, 1, -1, 0, 1 };
-
-            for (int i = 0; i < 8; i++)
+            if (currCell.Neighbors == 0)
             {
-                FloodFill(row + xmove[i], row + ymove[i]);
+                int[] xmove = { -1, -1, -1, 0, 0, 1, 1, 1 };
+                int[] ymove = { -1, -0, 1, -1, 1, -1, 0, 1 };
+
+                for (int i = 0; i < 8; i++)
+                {
+                    FloodFill(row + xmove[i], col + ymove[i]);
+                }
             }
         }
     }
